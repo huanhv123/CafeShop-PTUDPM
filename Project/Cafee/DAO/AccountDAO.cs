d601e90f642e064253b4a1623446bedff7ccf77f
tranvanhao016a <hao.tv1665@sinhvien.hoasen.edu.vn>
2022-10-16 01:00:36 +0700

    Admin

--- Project/Cafee/DAO/AccountDAO.cs
+++ Project/Cafee/DAO/AccountDAO.cs
@@ -5,11 +5,14 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Cafee.DAO
 {
     internal class AccountDAO
     {
+        String strCon = ConfigurationManager.ConnectionStrings["strCon"].ConnectionString;
         private static AccountDAO instance;
 
         public static AccountDAO Instance
@@ -68,5 +71,119 @@ namespace Cafee.DAO
         //    con.Close();
         //    return laptop;
         //}
+        public List<Account> SelectAll()
+        {
+            List<Account> list = new List<Account>();
+            SqlConnection con = new SqlConnection(strCon);
+            con.Open();
+            String strCom = "select * from Account";
+            SqlCommand com = new SqlCommand(strCom, con);
+            SqlDataReader dr = com.ExecuteReader();
+            while (dr.Read())
+            {
+                Account item = new Account()
+                {
+                    id = (int)dr["ID"],
+                    username = (string)dr["username"],
+                    displayName = (string)dr["displayName"],
+                    password = (string)dr["password"],
+                    type = (int)dr["type"],
+
+                };
+                list.Add(item);
+            }
+            con.Close();
+            return list;
+        }
+        public Account SelectedByCode(int ID)
+        {
+            Account account = null;
+            SqlConnection con = new SqlConnection(strCon);
+            con.Open();
+            String strCom = "select * from Account where id=@id";
+            SqlCommand com = new SqlCommand(strCom, con);
+            com.Parameters.Add(new SqlParameter("@id", ID));
+            SqlDataReader dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                account = new Account()
+                {
+                    id = (int)dr["ID"],
+                    username = (string)dr["username"],
+                    displayName = (string)dr["displayName"],
+                    password = (string)dr["password"],
+                    type = (int)dr["type"],
+                };
+            }
+            con.Close();
+            return account;
+        }
+        public bool Insert(Account newAccount)
+        {
+            bool result = false;
+            SqlConnection con = new SqlConnection(strCon);
+            con.Open();
+            String strCom = "INSERT INTO Account(username,displayName,password,type) VALUES(@username,@displayName,@password,@type)";
+            SqlCommand com = new SqlCommand(strCom, con);
+            com.Parameters.Add(new SqlParameter("@ID", newAccount.id));
+            com.Parameters.Add(new SqlParameter("@username", newAccount.username));
+            com.Parameters.Add(new SqlParameter("@displayName", newAccount.displayName));
+            com.Parameters.Add(new SqlParameter("@password", newAccount.password));
+            com.Parameters.Add(new SqlParameter("@type", newAccount.type));
+           
+
+            try
+            {
+                result = com.ExecuteNonQuery() > 0;
+            }
+            catch
+            {
+                result = false;
+            }
+            con.Close();
+            return result;
+        }
+        public bool Update(Account newAccount)
+        {
+            bool result = false;
+            SqlConnection con = new SqlConnection(strCon);
+            con.Open();
+            String strCom = "UPDATE Account SET displayName=@displayName,type=@type where id=@ID";
+            SqlCommand com = new SqlCommand(strCom, con);
+            //com.Parameters.Add(new SqlParameter("@username", newAccount.username));
+            com.Parameters.Add(new SqlParameter("@displayName", newAccount.displayName));
+            com.Parameters.Add(new SqlParameter("@type",newAccount.type));
+            com.Parameters.Add(new SqlParameter("@ID", newAccount.id));
+            try
+            {
+                result = com.ExecuteNonQuery() > 0;
+            }
+            catch
+            {
+                result = false;
+            }
+            con.Close();
+            return result;
+
+        }
+        public bool Delete(int ID)
+        {
+            bool result = false;
+            SqlConnection con = new SqlConnection(strCon);
+            con.Open();
+            String strCom = "DELETE FROM Account where id=@id";
+            SqlCommand com = new SqlCommand(strCom, con);
+            com.Parameters.Add(new SqlParameter("@id", ID));
+            try
+            {
+                result = com.ExecuteNonQuery() > 0;
+            }
+            catch
+            {
+                result = false;
+            }
+            con.Close();
+            return result;
+        }
     }
 }
